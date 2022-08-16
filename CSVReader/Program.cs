b7e29a92a4242d78950c1f854f4a1ec622565755
Rockandrolldream <sa.jba@specialisterne.com>
2022-08-16 10:07:05 +0200

    add

---
+++ CSVReader/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSVReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test");
+        }
+    }
+}
