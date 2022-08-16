d8d56149dd45231b2fd2568c70fa3c3f633f3987
Rockandrolldream <sa.jba@specialisterne.com>
2022-08-16 14:20:12 +0200

    first commit

--- CSVReader/Program.cs
+++ CSVReader/Program.cs
@@ -1,27 +1,29 @@
-﻿namespace CSVReader
+﻿using CSVReader.Model;
+
+namespace CSVReader
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            BallingdatabaseContext context = new BallingdatabaseContext();
+            IQueryable<Cereal> output;
             using (var reader = new StreamReader(@"C:/Users/KOM/source/repos/Apiopgave/CSVReader/Csvfile/cereal_unclean.csv"))
             {
-                List<string> name = new List<string>();
-                List<string> mfr = new List<string>();
-                List<string> type = new List<string>();
-                List<string> calories = new List<string>();
-                List<string> protein = new List<string>();
-                List<string> fat = new List<string>();
-                List<string> sodium = new List<string>(); 
-                List<string> fiber = new List<string>();
-                List<string> carbo = new List<string>();
+               
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(';');
                     Console.WriteLine(values[0]);
-                    name.Add(values[0]);
-                    mfr.Add(values[1]);
+
+                    output = context.Cereals.Where(m => m.Name == values[0]);
+                    if (output == null)
+                    {
+                        Cereal cereal = new Cereal() { Name = values[0] , Mfr = values[1] , Type = values[2] , Calories = values[3] , Protein = values[4] };
+                    }
+
+
                 }
             }
         }
