15217a989e5a0f38c6b407b2cd85ddbde606bde8
Rockandrolldream <sa.jba@specialisterne.com>
2022-08-16 11:51:39 +0200

    test

---
+++ CSVReader/Model/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSVReader.Model
+{
+    public partial class Token
+    {
+        public string? Token1 { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
--- CSVReader/Program.cs
+++ CSVReader/Program.cs
@@ -4,8 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("test");
+            using (var reader = new StreamReader(@"C:/Users/KOM/source/repos/Apiopgave/CSVReader/Csvfile/cereal_unclean.csv"))
+            {
+                List<string> name = new List<string>();
+                List<string> mfr = new List<string>();
+                List<string> type = new List<string>();
+                List<string> calories = new List<string>();
+                List<string> protein = new List<string>();
+                List<string> fat = new List<string>();
+                List<string> sodium = new List<string>(); 
+                List<string> fiber = new List<string>();
+                List<string> carbo = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    Console.WriteLine(values[0]);
+                    name.Add(values[0]);
+                    mfr.Add(values[1]);
+                }
+            }
         }
     }
 }
